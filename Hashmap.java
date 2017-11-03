127ec7a271dc011ed16745fb649b981b3ea66e3e
TheGentlemanCoder <santasmith123@hotmail.com>
2017-11-02 19:54:19 -0700

    Add empty Hashmap class for testing

---
+++ Hashmap.java
@@ -0,0 +1,36 @@
+package com.greyhounds;
+
+import java.util.ArrayList;
+
+public class Hashmap {
+        private ArrayList<ArrayList<KeyValuePair>> hashmapArray;
+        private int num_buckets = 256;
+        
+        public Hashmap() {
+                hashmapArray = getHashmap();
+        }
+        
+        private ArrayList<ArrayList<KeyValuePair>> getHashmap() {
+        
+        }
+        
+        public void set(Object key, Object value) {
+
+        }
+        
+        public Object get(Object key) {         
+
+        }
+        
+        private int getIndexFor(ArrayList<KeyValuePair> bucket, Object key) {
+
+        }
+        
+        private ArrayList<KeyValuePair> getBucket(Object key) {
+
+        }
+        
+        private int hashKey(Object key) {
+
+        }
+}
